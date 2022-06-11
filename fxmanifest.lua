5b8e83b527e0d0ef84365921ff541e2def0a9f10
Fistsofury <92045818+Fistsofury@users.noreply.github.com>
2022-06-11 11:22:25 +0100

    FXmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+
+author 'Fistsofury'
+description 'Fistsofury : Campfire Crafting'
+version '0.0.1'
+
+client_script {
+    'client/client.lua',
+	'config.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
